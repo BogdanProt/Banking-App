10cec06339bda2d2d5ed79bd66e60e1cf8308879
Bogdan Protopopescu <bodipp@Bogdans-MacBook-Air.local>
2024-04-02 18:04:13 +0300

    Finished Address class, added to do

---
+++ src/model/Address.java
@@ -0,0 +1,84 @@
+package model;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Address {
+    private String country;
+    private String county;
+    private String city;
+    private String street;
+    private int postalCode;
+
+    public Address(String country, String county, String city, String street, int postalCode){
+        this.country = country;
+        this.county = county;
+        this.city = city;
+        this.street = street;
+        this.postalCode = postalCode;
+    }
+
+    public Address(Scanner in) {
+        this.read(in);
+    }
+
+    public void read (Scanner in) {
+        System.out.println("Country: ");
+        country = in.nextLine().toLowerCase();
+        System.out.println("County: ");
+        county = in.nextLine().toLowerCase();
+        System.out.println("City: ");
+        city = in.nextLine().toLowerCase();
+        System.out.println("Street: ");
+        street = in.nextLine().toLowerCase();
+        System.out.println("Postal code: ");
+        postalCode = in.nextInt();
+    }
+
+    public String getCountry() { return country; }
+
+    public String getCounty() {
+        return county;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setCounty(String county) {
+        this.county = county;
+    }
+
+    public void setCity(String city) { this.city = city; }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public void setPostalCode(int postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "country='" + country + '\'' +
+                ", county='" + county + '\'' +
+                ", city='" + city + '\'' +
+                ", street='" + street + '\'' +
+                ", postalCode=" + postalCode +
+                '}';
+    }
+}
---
+++ src/model/User.java
@@ -0,0 +1,14 @@
+package model;
+
+public class User {
+    private int userID;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String CNP;
+    private String phoneNumber;
+    private Address address;
+
+
+}
