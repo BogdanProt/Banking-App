b151578e37662275ddfc1d09ff2c9b9afb12611b
Bogdan Protopopescu <bodipp@Bogdans-MacBook-Air.local>
2024-04-05 18:40:56 +0300

    Added MasterCard and Visa

---
+++ src/model/card/MasterCard.java
@@ -0,0 +1,8 @@
+package model.card;
+import model.card.Card;
+
+public class MasterCard extends Card {
+    public MasterCard(int cardID, String IBAN, String name){
+        super(cardID, IBAN, name);
+    }
+}
---
+++ src/model/card/Visa.java
@@ -0,0 +1,8 @@
+package model.card;
+import model.card.Card;
+
+public class Visa extends Card{
+    Visa(int cardID, String IBAN, String name){
+        super(cardID, IBAN, name);
+    }
+}
