99e90c86062afc45bcdb1a2e52449617cc233723
Bogdan Protopopescu <bodipp@Bogdans-MacBook-Air.local>
2024-04-05 19:09:23 +0300

    Added Card util, fixed Visa bug

--- src/model/card/Visa.java
+++ src/model/card/Visa.java
@@ -2,7 +2,7 @@ package model.card;
 import model.card.Card;
 
 public class Visa extends Card{
-    Visa(int cardID, String IBAN, String name){
+    public Visa(int cardID, String IBAN, String name){
         super(cardID, IBAN, name);
     }
 }
---
+++ src/utils/CardSeparation.java
@@ -0,0 +1,21 @@
+package utils;
+import model.card.Card;
+import model.card.MasterCard;
+import model.card.Visa;
+
+public class CardSeparation {
+    private static int uniqueID = 0;
+
+    public Card addCard(String IBAN, String name) {
+        return new Card(uniqueID++, IBAN, name);
+    }
+
+    public MasterCard createMasterCard(String IBAN, String name) {
+        return new MasterCard(uniqueID++, IBAN, name);
+    }
+
+    public Visa createVisaCard(String IBAN, String name) {
+        return new Visa(uniqueID++, IBAN, name);
+    }
+
+}
