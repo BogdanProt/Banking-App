6c4667bd4b70d432af7d6539739861f2fae1bb32
Bogdan Protopopescu <bodipp@Bogdans-MacBook-Air.local>
2024-04-06 13:09:27 +0300

    Added Transaction DAO

---
+++ src/dao/TransactionDAO.java
@@ -0,0 +1,31 @@
+package dao;
+
+import model.account.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TransactionDAO {
+
+    private static List<Transaction> transactions = new ArrayList<>();
+
+    public Transaction read(Date date) {
+        if (!transactions.isEmpty()) {
+            for (Transaction trans : transactions) {
+                if (date == trans.getIssuedDate()) {
+                    return trans;
+                }
+            }
+        }
+        return null;
+    }
+
+    public void create(Transaction trans) {
+        transactions.add(trans);
+    }
+
+    public void delete(Transaction trans) {
+        transactions.remove(trans);
+    }
+}
