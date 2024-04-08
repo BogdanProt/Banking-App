62963b724e2de52d2fa10983d73833808fba7602
Bogdan Protopopescu <bodipp@Bogdans-MacBook-Air.local>
2024-04-08 13:16:26 +0300

    Finished UserRepoService

---
+++ src/daoservices/UserRepositoryService.java
@@ -0,0 +1,48 @@
+package daoservices;
+
+import dao.*;
+import model.card.*;
+import model.account.*;
+import model.user.*;
+import utils.*;
+
+public class UserRepositoryService {
+
+    private UserDAO userDAO;
+
+    public UserRepositoryService() {
+        this.userDAO = new UserDAO();
+    }
+
+    public User getUserByID(int userID) {
+        User user = userDAO.read(userID);
+
+        if (user != null) {
+            System.out.println(user);
+        } else {
+            System.out.println("No user having this ID!");
+        }
+
+        return user;
+    }
+
+    public void addUser(User user) {
+        if (user != null) {
+            userDAO.create(user);
+        } else {
+            throw new IllegalStateException("Unexpected value: " + user);
+        }
+
+        System.out.println("Added " + user);
+    }
+
+    public void removeUser(User user) {
+        if (user != null) {
+            userDAO.delete(user);
+        } else {
+            throw new IllegalStateException("Unexpected value: " + user);
+        }
+
+        System.out.println("Removed " + user);
+    }
+}
