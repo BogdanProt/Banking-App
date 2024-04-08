dacb717ef1518fae8dc2500c69b4c5ac35a5ddd8
Bogdan Protopopescu <bodipp@Bogdans-MacBook-Air.local>
2024-04-08 13:10:34 +0300

    Finished TransactionRepoService

---
+++ src/daoservices/TransactionRepositoryService.java
@@ -0,0 +1,51 @@
+package daoservices;
+
+import dao.*;
+import model.card.*;
+import model.account.*;
+import model.user.*;
+import utils.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TransactionRepositoryService {
+
+    private TransactionDAO transactionDAO;
+
+    public TransactionRepositoryService() {
+        this.transactionDAO = new TransactionDAO();
+    }
+
+    public Transaction getTransactionByDate(Date date) {
+        Transaction transaction = transactionDAO.read(date);
+
+        if (transaction != null) {
+            System.out.println(transaction);
+        } else {
+            System.out.println("No transaction having this exact date. Please check the date again!");
+        }
+
+        return transaction;
+    }
+
+    public void addTransaction(Transaction transaction) {
+        if (transaction != null) {
+            transactionDAO.create(transaction);
+        } else {
+            throw new IllegalStateException("Unexpected value: " + transaction);
+        }
+
+        System.out.println("Added " + transaction);
+    }
+
+    public void removeTransaction(Transaction transaction) {
+        if (transaction != null) {
+            transactionDAO.delete(transaction);
+        } else {
+            throw new IllegalStateException("Unexpected value: " + transaction);
+        }
+
+        System.out.println("Removed: " + transaction);
+    }
+
+}
