bb9e52e9ca5ede94e8e05cdf166af1019e2a1d0b
BogdanProt <bprotopopescu09@gmail.com>
2024-05-01 14:58:02 +0300

    Added connection and sql script

---
+++ src/scripts/script.sql
@@ -0,0 +1,64 @@
+CREATE TABLE `Banking`.`Account` (
+    `IBAN` VARCHAR(45) NOT NULL,
+    `swift` VARCHAR(45) NULL,
+    `balance` FLOAT NULL,
+    `name` VARCHAR(45) NULL,
+    `userID` INT NOT NULL,
+    PRIMARY KEY (`IBAN`)
+);
+
+CREATE TABLE `Banking`.`SavingsAccount` (
+    `IBAN` VARCHAR(45) NOT NULL,
+    `swift` VARCHAR(45) NULL,
+    `balance` FLOAT NULL,
+    `name` VARCHAR(45) NULL,
+    `userID` INT NOT NULL,
+    `startDate` DATE NULL,
+    `endDate` DATE NULL,
+    `interestRate` INT NULL,
+    PRIMARY KEY (`IBAN`)
+);
+
+CREATE TABLE `Banking`.`Transaction` (
+    `fromIBAN` VARCHAR(45) NOT NULL,
+    `toIBAN` VARCHAR(45) NOT NULL,
+    `description` VARCHAR(45) NULL,
+    `amount` FLOAT NULL,
+    `issuedDate` DATE,
+    PRIMARY KEY (`fromIBAN`, `toIBAN`)
+);
+
+CREATE TABLE `Banking`.`User` (
+    `userID` INT NOT NULL,
+    `firstName` VARCHAR(45) NULL,
+    `lastName` VARCHAR(45) NULL,
+    `CNP` VARCHAR(45) NULL,
+    `emailAddress` VARCHAR(45) NULL,
+    `phoneNumber` VARCHAR(45) NULL,
+    `country` VARCHAR(45) NULL,
+    `county` VARCHAR(45) NULL,
+    `city` VARCHAR(45) NULL,
+    `street` VARCHAR(45) NULL,
+    `postalCode` INT NULL,
+    PRIMARY KEY (`userID`)
+);
+
+CREATE TABLE `Banking`.`MasterCard` (
+    `cardID` INT NOT NULL,
+    `CVV` INT NULL,
+    `cardNumber` VARCHAR(45) NULL,
+    `name` VARCHAR(45) NULL,
+    `IBAN` VARCHAR(45) NULL,
+    `expirationDate` DATE NULL,
+    PRIMARY KEY (cardID)
+);
+
+CREATE TABLE `Banking`.`Visa` (
+    `cardID` INT NOT NULL,
+    `CVV` INT NULL,
+    `cardNumber` VARCHAR(45) NULL,
+    `name` VARCHAR(45) NULL,
+    `IBAN` VARCHAR(45) NULL,
+    `expirationDate` DATE NULL,
+    PRIMARY KEY (cardID)
+);
---
+++ src/utils/Constants.java
@@ -0,0 +1,7 @@
+package utils;
+
+public class Constants {
+    public static final String JDBC_DRIVER = "jdbc:mysql://localhost/Banking";
+    public static final String JDBC_PWD = "Password1!";
+    public static final String JDBC_USER = "root";
+}
